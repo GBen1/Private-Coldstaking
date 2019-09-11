0ac160863b86d4fd93207b2ff2884472725fc8e4
GBen1 <31903813+GBen1@users.noreply.github.com>
2019-09-11 09:12:44 +0200

    Update privatecoldstaking.sh

--- privatecoldstaking.sh
+++ privatecoldstaking.sh
@@ -2,7 +2,7 @@ yel='\e[1;33m'
 neutre='\e[0;m'
 
 
-cd && cd particlcore && echo -e "${yel}Enter a public address generated from your desktop/qt/copat wallet where you want to receive your anonymized coins:${neutre}" && read wallet
+cd && cd particlcore && echo -e "${yel}Enter a public address generated from your Desktop/Qt/Copay wallet, this address will be the reception address for your anonymized rewards:${neutre}" && read wallet
 
 rewardaddress=$(cd && cd particlcore && ./particl-cli getnewaddress) 
 
@@ -12,7 +12,7 @@ cd && cd particlcore && stealthaddressnode=$(./particl-cli getnewstealthaddress)
 
 echo "$wallet" > wallet.txt 
 
-echo -e "${yel}Your rewards are going to be anonymized from this address: $rewardaddress and then they will be sent back to you on this address: $wallet ${neutre}"
+echo -e "${yel}Your rewards are going to be anonymized on your coldstakingnode (from this address: $rewardaddress), afterward they will be automatically sent back to you on this address: $wallet ${neutre}"
 
 echo "bash -c 'while true;do ./particl-cli settxfee 0.002 && random=$(echo $(( RANDOM % (100 - 50 + 1 ) + 50 ))) && stealthaddressnode=$(cat stealthaddressnode.txt) && ./particl-cli sendparttoanon $stealthaddressnode 0.45; sleep $random ; done' " > script1.sh
 
