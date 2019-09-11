712ec2140890dbca9772c8c91e758a8615b8c250
GBen1 <31903813+GBen1@users.noreply.github.com>
2019-09-11 08:17:37 +0200

    Update privatecoldstaking.sh

--- privatecoldstaking.sh
+++ privatecoldstaking.sh
@@ -7,14 +7,3 @@ echo "enter a public address generated from your desktop/qt/copat wallet where y
 read wallet
 echo "$wallet" > wallet.txt 
 }
-
-private(){
-rewardaddress=$(./particl-cli getnewaddress)
-./particl-cli walletsettings stakingoptions "{\"rewardaddress\":\"$rewardaddress\"}"
-stealthaddressnode=$(./particl-cli getnewstealthaddress) 
-echo "$stealthaddressnode" > stealthaddressnode.txt
-echo "bash -c 'while true;do ./particl-cli settxfee 0.002 && random=$(echo $(( RANDOM % (100 - 50 + 1 ) + 50 ))) && stealthaddressnode=$(cat stealthaddressnode.txt) && ./particl-cli sendparttoanon $stealthaddressnode 0.45; sleep $random ; done' " > script1.sh
-echo "bash -c 'while true;do ./particl-cli settxfee 0.002 && random=$(echo $(( RANDOM % (100 - 50 + 1 ) + 50 ))) && wallet=$(cat wallet.txt) && ./particl-cli sendanontopart $wallet 0.4; sleep $random; done'" > script2.sh
-nohup bash script1.sh & nohup bash script2.sh
-}
-
