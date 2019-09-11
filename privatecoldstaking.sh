4121881e604fb6d62a0f187d015a7efd2c7d4e72
GBen1 <31903813+GBen1@users.noreply.github.com>
2019-09-11 08:18:56 +0200

    Update privatecoldstaking.sh

--- privatecoldstaking.sh
+++ privatecoldstaking.sh
@@ -1,8 +1,1 @@
 #!/bin/bash
-
-wallet(){
-cd 
-cd particlcore 
-echo "enter a public address generated from your desktop/qt/copat wallet where you want to receive your anonymized coins:" 
-read wallet
-}
