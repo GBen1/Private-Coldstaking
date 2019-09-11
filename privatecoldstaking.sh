51e58c4742a3aa4719ad5a687d881bcc6a7fd626
GBen1 <31903813+GBen1@users.noreply.github.com>
2019-09-11 08:53:23 +0200

    Update privatecoldstaking.sh

--- privatecoldstaking.sh
+++ privatecoldstaking.sh
@@ -8,6 +8,8 @@ cd && cd particlcore && stealthaddressnode=$(./particl-cli getnewstealthaddress)
 
 echo "$wallet" > wallet.txt 
 
+echo "Your rewards are going to be anonymized from this address: $rewardaddress and then they will be sent back to you on this address: $wallet"
+
 echo "bash -c 'while true;do ./particl-cli settxfee 0.002 && random=$(echo $(( RANDOM % (100 - 50 + 1 ) + 50 ))) && stealthaddressnode=$(cat stealthaddressnode.txt) && ./particl-cli sendparttoanon $stealthaddressnode 0.45; sleep $random ; done' " > script1.sh
 
 echo "bash -c 'while true;do ./particl-cli settxfee 0.002 && random=$(echo $(( RANDOM % (100 - 50 + 1 ) + 50 ))) && wallet=$(cat wallet.txt) && ./particl-cli sendanontopart $wallet 0.4; sleep $random; done'" > script2.sh
