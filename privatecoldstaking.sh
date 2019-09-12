d4048c572f903086c3ce76ab9cb7b7aba5f541e3
GBen1 <31903813+GBen1@users.noreply.github.com>
2019-09-12 20:27:13 +0200

    Update privatecoldstaking.sh

--- privatecoldstaking.sh
+++ privatecoldstaking.sh
@@ -6,19 +6,19 @@ cd
 
 script1=$(ps -ef | grep bash | grep script1.sh | cut -c10-14)
 num=$(echo $script1 | wc -w)
-x=1; while [ $x -le $num ]; do kill=$(ps -ef | grep bash | grep script1.sh | cut -c10-14 | sed -n "1p") && kill -9 $kill $(( x++ )); done
+x=1; while [ $x -le $num ]; do kill=$(ps -ef | grep bash | grep script1.sh | cut -c10-14 | sed -n "1p") && sudo kill -9 $kill $(( x++ )); done
 
 script2=$(ps -ef | grep bash | grep script2.sh | cut -c10-14)
 num=$(echo $script2 | wc -w)
-x=1; while [ $x -le $num ]; do kill=$(ps -ef | grep bash | grep script2.sh | cut -c10-14 | sed -n "1p") && kill -9 $kill $(( x++ )); done
+x=1; while [ $x -le $num ]; do kill=$(ps -ef | grep bash | grep script2.sh | cut -c10-14 | sed -n "1p") && sudo kill -9 $kill $(( x++ )); done
 
 parttoanon=$(ps -ef | grep bash | grep sendparttoanon | cut -c10-14)
 num=$(echo $parttoanon | wc -w)
-x=1; while [ $x -le $num ]; do kill=$(ps -ef | grep bash | grep sendparttoanon | cut -c10-14 | sed -n "1p") && kill -9 $kill $(( x++ )); done
+x=1; while [ $x -le $num ]; do kill=$(ps -ef | grep bash | grep sendparttoanon | cut -c10-14 | sed -n "1p") && sudo kill -9 $kill $(( x++ )); done
 
 anontopart=$(ps -ef | grep bash | grep anontopart | cut -c10-14)
 num=$(echo $anontopart | wc -w)
-x=1; while [ $x -le $num ]; do kill=$(ps -ef | grep bash | grep sendanontopart | cut -c10-14 | sed -n "1p") && kill -9 $kill $(( x++ )); done
+x=1; while [ $x -le $num ]; do kill=$(ps -ef | grep bash | grep sendanontopart | cut -c10-14 | sed -n "1p") && sudo kill -9 $kill $(( x++ )); done
 
 sudo apt-get update && sudo apt-get upgrade <<< y
 
