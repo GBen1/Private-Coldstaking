07d11850eeb1cc440a376b83204774323141fc32
GBen1 <31903813+GBen1@users.noreply.github.com>
2019-09-15 21:24:59 +0200

    Create cryptoguard.sh

---
+++ cryptoguard.sh
@@ -0,0 +1,22 @@
+cd
+cd particlcore
+
+csbalance=$(./particl-cli getcoldstakinginfo | grep coin_in_cold| cut -c34- | rev | cut -c2- | rev | sed 's/ //')
+echo "csbalance "$csbalance"
+
+csbal=$(echo $csbalance | cut -d "." -f 1 | cut -d "," -f 1)
+echo "csbal $csbal"
+
+ratio1=0.00007
+
+entro=$(awk -v seed="$RANDOM" 'BEGIN { srand(seed);  printf("%.4f\n", rand()) }')
+entro=$(printf '%.3f\n' "$(echo "$entro" | bc -l)")
+entro=$(printf '%.3f\n' "$(echo "$entro" "*" "1000" | bc -l)")
+entro=$(printf '%.3f\n' "$(echo "$entro" "+" "1000" | bc -l)")
+entro=$(echo "$entro" | cut -d "." -f 1 | cut -d "," -f 1)
+
+echo "entropie $entro"
+
+amount1=$(printf '%.3f\n' "$(echo "$csbal" "*" "$ratio1" "*" "$entro" | bc -l)")
+
+echo "amount = csbal x ratio1 x entropie = $amount1"
