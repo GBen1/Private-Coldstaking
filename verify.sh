d0f89630dd7ca6683c5357d7b9ac3b8639e4b56d
GBen1 <31903813+GBen1@users.noreply.github.com>
2019-09-15 22:31:44 +0200

    Update verify.sh

--- verify.sh
+++ verify.sh
@@ -5,5 +5,7 @@ ps -ef | grep bash | grep parttoanon | cut -c49-
 echo ""
 ps -ef | grep bash | grep anontopart | cut -c49-
 echo ""
+ps -ef | grep bash | grep anontoanon | cut -c49-
+echo ""
 ps -ef | grep bash | grep anontoblind | cut -c49-
 echo ""
