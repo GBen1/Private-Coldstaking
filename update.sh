26a3712816351d45d1c725df819c50ad1eaf6dbb
GBen1 <31903813+GBen1@users.noreply.github.com>
2019-09-15 18:21:01 +0200

    Update update.sh

--- update.sh
+++ update.sh
@@ -2,6 +2,7 @@ neutre='\e[0;m'
 gr='\e[1;32m'
 cy='\e[0;36m'
 bl='\e[1;36m'
+yel='\e[1;33m'
 
 git pull
 cd
@@ -21,20 +22,20 @@ read -p "Press [Enter] key to continue..."
 clear
 cd
 cd particlcore
-echo -e "${bl}NODE BALANCES${neutre}"
+echo -e "${gr}NODE BALANCES${neutre}"
 echo ""
 balances=$(./particl-cli getwalletinfo | sed -n '4,/tx/p' | sed "11d" | sed 's/"//' | sed 's/"//' |sed 's/,//' | cut -c3- | rev | cut -c6- | rev)
-echo -e "${cy}$balances${neutre}"
+echo -e "${yel}$balances${neutre}"
 echo ""
 echo ""
-echo -e "${bl}ACTIVE SCRIPTS${neutre}"
+echo -e "${gr}ACTIVE SCRIPTS${neutre}"
 echo ""
 parttoanon=$(ps -ef | grep bash | grep parttoanon | cut -c49-)
 echo ""
-echo -e "${cy}$parttoanon${neutre}"
+echo -e "${yel}$parttoanon${neutre}"
 anontopart=$(ps -ef | grep bash | grep anontopart | cut -c49-)
 echo ""
-echo -e "${cy}$anontopart${neutre}"
+echo -e "${yel}$anontopart${neutre}"
 anontoblind=$(ps -ef | grep bash | grep anontoblind | cut -c49-)
 echo ""
-echo -e "${cy}$anontoblind${neutre}"
+echo -e "${yel}$anontoblind${neutre}"
