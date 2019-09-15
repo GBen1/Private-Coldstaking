d208d073d1af6920b37b8f83fc851063c2804513
GBen1 <31903813+GBen1@users.noreply.github.com>
2019-09-15 18:59:21 +0200

    Update update.sh

--- update.sh
+++ update.sh
@@ -27,19 +27,16 @@ contractprivatecs=$(cat contractprivatecs.txt | sed "1,2d")
 clear
 echo -e "${gr}NODE BALANCES${neutre}"
 echo ""
-echo ""
 balances=$(./particl-cli getwalletinfo | sed -n '4,/tx/p' | sed "11d" | sed 's/"//' | sed 's/"//' |sed 's/,//' | sed 's/txcount//' | cut -c3- | rev | cut -c6- | rev | tac | sed "1d" | tac)
 echo -e "${yel}$balances${neutre}"
 echo ""
 echo ""
 echo -e "${gr}PRIVATE COLDSTAKING CONTRACT${neutre}"
-echo ""
 echo -e "${yel}$contract${neutre}"
 echo -e "${yel}$contractprivatecs${neutre}"
 echo ""
 echo ""
 echo -e "${gr}ACTIVE SCRIPTS${neutre}"
-echo ""
 parttoanon=$(ps -ef | grep bash | grep parttoanon | cut -c49-)
 echo ""
 echo -e "${yel}$parttoanon${neutre}"
