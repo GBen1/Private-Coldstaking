3d74273a3afb101254dee7e5204c46bb4368cca0
GBen1 <31903813+GBen1@users.noreply.github.com>
2019-09-15 18:56:19 +0200

    Update update.sh

--- update.sh
+++ update.sh
@@ -27,7 +27,8 @@ contractprivatecs=$(cat contractprivatecs.txt | sed "1,2d")
 clear
 echo -e "${gr}NODE BALANCES${neutre}"
 echo ""
-balances=$(./particl-cli getwalletinfo | sed -n '4,/tx/p' | sed "11d" | sed 's/"//' | sed 's/"//' |sed 's/,//' | cut -c3- | rev | cut -c6- | rev)
+balances=$(./particl-cli getwalletinfo | sed -n '4,/tx/p' | sed "11d" | sed 's/"//' | sed 's/"//' |sed 's/,//' | sed 's/txcount//' | cut -c3- | rev | cut -c6- | rev | tac | sed "1d" | tac
+)
 echo -e "${yel}$balances${neutre}"
 echo ""
 echo ""
