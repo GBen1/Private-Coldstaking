431cad4c8366af04acc8bbb6e5a2921f7008e622
GBen1 <31903813+GBen1@users.noreply.github.com>
2019-09-15 18:02:53 +0200

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,6 +1,7 @@
 neutre='\e[0;m'
 gr='\e[1;32m'
 cy='\e[0;36m'
+bl='\e[1;36m'
 
 git pull
 cd
@@ -20,8 +21,8 @@ read -p "Press [Enter] key to continue..."
 clear
 cd
 cd particlcore
-echo -e "${cy}NODE BALANCES${neutre}"
+echo -e "${bl}NODE BALANCES${neutre}"
 echo ""
 balances=$(./particl-cli getwalletinfo | sed -n '4,/tx/p' | sed "11d" | sed 's/"//' | sed 's/"//' |sed 's/,//' | cut -c3- | rev | cut -c6- | rev)
-echo -e "${cy}$balances${neutre}"
+echo -e "${bl}$balances${neutre}"
 echo ""
