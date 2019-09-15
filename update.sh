9ef74770f21d7333e80a2d4dc533785b43f8ed82
GBen1 <31903813+GBen1@users.noreply.github.com>
2019-09-15 18:28:13 +0200

    Update update.sh

--- update.sh
+++ update.sh
@@ -5,6 +5,7 @@ bl='\e[1;36m'
 yel='\e[1;33m'
 
 git pull
+contract=$(cat contract.txt]
 cd
 cd partyman
 clear
@@ -22,10 +23,18 @@ read -p "Press [Enter] key to continue..."
 clear
 cd
 cd particlcore
+contractprivatecs=$(cat contractprivatecs.txt)
+clear
 echo -e "${gr}NODE BALANCES${neutre}"
 echo ""
 balances=$(./particl-cli getwalletinfo | sed -n '4,/tx/p' | sed "11d" | sed 's/"//' | sed 's/"//' |sed 's/,//' | cut -c3- | rev | cut -c6- | rev)
 echo -e "${yel}$balances${neutre}"
+echo ""
+echo ""
+echo -e "${gr}PRIVATE COLDSTAKING CONTACT${neutre}"
+echo ""
+echo -e "${yel} $contract ${neutre}"
+echo -e "${yel} $contractprivatecs ${neutre}"
 echo ""
 echo ""
 echo -e "${gr}ACTIVE SCRIPTS${neutre}"
