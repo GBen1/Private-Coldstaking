e3276015fa2e81aab4259eab14fb715bacd7e2c4
GBen1 <31903813+GBen1@users.noreply.github.com>
2019-09-15 18:07:24 +0200

    Update update.sh

--- update.sh
+++ update.sh
@@ -25,4 +25,7 @@ echo -e "${bl}NODE BALANCES${neutre}"
 echo ""
 balances=$(./particl-cli getwalletinfo | sed -n '4,/tx/p' | sed "11d" | sed 's/"//' | sed 's/"//' |sed 's/,//' | cut -c3- | rev | cut -c6- | rev)
 echo -e "${bl}$balances${neutre}"
+verify=$(verify.sh)
+echo ""
+echo "$verify"
 echo ""
