d30bea737cdf0369ba0e48ea29f70b7170900029
GBen1 <31903813+GBen1@users.noreply.github.com>
2019-09-16 23:19:58 +0200

    Update verify.sh

--- verify.sh
+++ verify.sh
@@ -3,11 +3,38 @@
 clear
 echo "ACTIVE SCRIPTS"
 echo ""
+
+chps=$(ps -ef | grep bash | grep parttoanon | cut -c49- | wc -c)
+
+if ((chps > 1 ));
+then
 ps -ef | grep bash | grep parttoanon | cut -c49-
 echo ""
+fi
+
+
+chps=$(ps -ef | grep bash | grep anontopart | cut -c49- | wc -c)
+
+if ((chps > 1 ));
+then
 ps -ef | grep bash | grep anontopart | cut -c49-
 echo ""
-ps -ef | grep bash | grep anontoanon | cut -c49-
-echo ""
+fi
+
+
+chps=$(ps -ef | grep bash | grep anontoblind | cut -c49- | wc -c)
+
+if ((chps > 1 ));
+then
 ps -ef | grep bash | grep anontoblind | cut -c49-
 echo ""
+fi
+
+
+chps=$(ps -ef | grep bash | grep anontoanon | cut -c49- | wc -c)
+
+if ((chps > 1 ));
+then
+ps -ef | grep bash | grep anontoanon | cut -c49-
+echo ""
+fi
