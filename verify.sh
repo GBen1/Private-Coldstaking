f7ba8c084b664c66bfc2229aced7357a4c1b5359
GBen1 <31903813+GBen1@users.noreply.github.com>
2019-09-16 23:45:03 +0200

    Update verify.sh

--- verify.sh
+++ verify.sh
@@ -1,40 +1,45 @@
 #!/bin/bash
 
+neutre='\e[0;m'
+gr='\e[1;32m'
+yel='\e[1;33m'
+
 clear
-echo "ACTIVE SCRIPTS"
-echo ""
 
-chps=$(ps -ef | grep bash | grep parttoanon | cut -c49- | wc -c)
+echo -e "${gr}ACTIVE SCRIPTS${neutre}"
 
+parttoanon=$(ps -ef | grep bash | grep parttoanon | cut -c49-)
+chps=$(echo $parttoanon | wc -c)
 if ((chps > 1 ));
 then
-ps -ef | grep bash | grep parttoanon | cut -c49-
 echo ""
+echo -e "${yel}$parttoanon${neutre}"
+fi
+
+anontoanon=$(ps -ef | grep bash | grep anontoanon | cut -c49-)
+chps=$(echo $anontoanon | wc -c)
+if ((chps > 1 ));
+then
+echo ""
+echo -e "${yel}$anontoanon${neutre}"
 fi
 
 
-chps=$(ps -ef | grep bash | grep anontopart | cut -c49- | wc -c)
-
+anonttoblind=$(ps -ef | grep bash | grep anonttoblind | cut -c49-)
+chps=$(echo $anonttoblind | wc -c)
 if ((chps > 1 ));
 then
-ps -ef | grep bash | grep anontopart | cut -c49-
 echo ""
+echo -e "${yel}$anonttoblind${neutre}"
 fi
 
 
-chps=$(ps -ef | grep bash | grep anontoblind | cut -c49- | wc -c)
-
+anontopart=$(ps -ef | grep bash | grep anontopart | cut -c49-)
+chps=$(echo $anontopart| wc -c)
 if ((chps > 1 ));
 then
-ps -ef | grep bash | grep anontoblind | cut -c49-
 echo ""
+echo -e "${yel}$anontopart${neutre}"
 fi
 
-
-chps=$(ps -ef | grep bash | grep anontoanon | cut -c49- | wc -c)
-
-if ((chps > 1 ));
-then
-ps -ef | grep bash | grep anontoanon | cut -c49-
 echo ""
-fi
