1526ef561ff4c913067e50993824f08e9da9a7ad
GBen1 <31903813+GBen1@users.noreply.github.com>
2019-09-16 23:49:49 +0200

    Update verify.sh

--- verify.sh
+++ verify.sh
@@ -5,6 +5,38 @@ gr='\e[1;32m'
 yel='\e[1;33m'
 
 clear
+contract=$(cat contract.txt | sed "1,2d")
+
+cd
+cd particlcore
+[ -f contractprivatecs.txt ] && contractprivatecs=$(cat contractprivatecs.txt | sed "1,2d") 
+csb=$(./particl-cli getcoldstakinginfo | grep coin_in_cold | cut -c35-44 | cut -d "." -f 1 | cut -d "," -f 1)
+clear
+clear
+echo -e "${gr}COLDSTAKING BALANCE${neutre}"
+echo ""
+echo -e "${yel}$csb PARTS${neutre}"
+echo ""
+echo ""
+echo -e "${gr}NODE BALANCES${neutre}"
+echo ""
+network=$(./particl-cli getstakinginfo | sed 's/"//' | sed 's/"//' |sed 's/,//' | cut -c3-)
+balances=$(./particl-cli getwalletinfo | sed "1,3d" | tac | sed "1,9d" | tac | sed 's/"//' | sed 's/"//' |sed 's/,//' | cut -c3- | rev | cut -c6- | rev )
+echo -e "${yel}$balances${neutre}"
+echo ""
+echo ""
+echo -e "${gr}NETWORK INFOS${neutre}"
+echo -e "${yel}$network${neutre}"
+echo ""
+echo ""
+read -p "$(echo -e ${gr}Press [Enter] key to continue...${neutre})"
+clear
+clear
+echo -e "${gr}PRIVATE COLDSTAKING CONTRACT${neutre}"
+echo -e "${yel}$contract${neutre}"
+echo -e "${yel}$contractprivatecs${neutre}"
+echo ""
+echo ""
 
 echo -e "${gr}ACTIVE SCRIPTS${neutre}"
 
