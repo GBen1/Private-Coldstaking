fa0e79b8e6e7c316005470cdac4e09c1cf175453
GBen1 <31903813+GBen1@users.noreply.github.com>
2019-09-16 17:57:21 +0200

    Update update.sh

--- update.sh
+++ update.sh
@@ -25,7 +25,7 @@ read -p "$(echo -e ${gr}Press [Enter] key to continue...${neutre})"
 clear
 cd
 cd particlcore
-contractprivatecs=$(cat contractprivatecs.txt | sed "1,2d") 2>/dev/null
+contractprivatecs=$(cat contractprivatecs.txt | sed "1,2d") /dev/null 2>&1
 csb=$(./particl-cli getcoldstakinginfo | grep coin_in_cold | cut -c35-44 | cut -d "." -f 1 | cut -d "," -f 1)
 clear
 clear
