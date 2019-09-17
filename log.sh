51553b10a20646fa7160201474c3ab07c8d7260d
GBen1 <31903813+GBen1@users.noreply.github.com>
2019-09-17 08:11:22 +0200

    Update log.sh

--- log.sh
+++ log.sh
@@ -5,7 +5,11 @@ clear
 cd particlcore
 [ -f  errorscriptcs.txt ] && cat  errorscriptcs.txt
 echo ""
+echo "NOHUP.OUT"
+echo ""
 tail -n 10 nohup.out 
+echo ""
+echo "NOHUP.ERR"
 echo ""
 tail -n 10 nohup.err
 echo ""
