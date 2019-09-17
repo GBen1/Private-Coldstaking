c0b96cf5f286502f46c6f80606039b1c9e53838d
GBen1 <31903813+GBen1@users.noreply.github.com>
2019-09-17 13:47:56 +0200

    Update privatecoldstaking.sh

--- privatecoldstaking.sh
+++ privatecoldstaking.sh
@@ -8,7 +8,7 @@ bl='\e[1;36m'
 flred='\e[1;41m'
 
 clear
-readme=$(cat README.md | tac | sed "1,24d" | tac)
+readme=$(cat README.md | tac | sed "1,28d" | tac)
 echo -e "\033[40m\033[1m$readme\033[0m"
 echo ""
 echo ""
