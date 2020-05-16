7c820ba8c073113476e9be226f9ca9b56acd9e09
GBen1 <31903813+GBen1@users.noreply.github.com>
2020-05-16 21:55:06 +0200

    Update log.sh

--- log.sh
+++ log.sh
@@ -1,15 +1,23 @@
 #!/bin/bash
 
+flred='\e[1;41m'
+
 cd
 clear
 cd particlcore
 [ -f  errorscriptcs.txt ] && cat  errorscriptcs.txt
-echo "NOHUP.OUT"
+echo -e "${flred}NOHUP.OUT${neutre}"
 echo ""
 tail -n 10 nohup.out 
 echo ""
 echo ""
-echo "NOHUP.ERR"
+echo -e "${flred}NOHUP.ERR${neutre}"
 echo ""
 tail -n 10 nohup.err
 echo ""
+echo ""
+echo -e "${flred}DEBUG.LOG${neutre}"
+echo ""
+cd
+cd .particl
+tail -n 10 debug.log
