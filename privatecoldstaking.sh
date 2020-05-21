f0a2a618901b83efbe07661bf1fdcb9b5ada1aa9
GBen1 <31903813+GBen1@users.noreply.github.com>
2020-05-21 07:52:44 +0200

    Update privatecoldstaking.sh

--- privatecoldstaking.sh
+++ privatecoldstaking.sh
@@ -66,10 +66,30 @@ echo ""
 echo ":: Updating repos, packages and installing dependencies.."
 echo ""
 
+#apt-get install sudo -y
+#sudo apt update -y && sudo apt -y upgrade
+#sudo apt install -y netcat-openbsd python git unzip pv jq dnsutils bc python-pip python-qrcode
+#sudo pip install qrcode[pil]
+
+apt install bc <<< y
+
 apt-get install sudo -y
-sudo apt update -y && sudo apt -y upgrade
-sudo apt install -y netcat-openbsd python git unzip pv jq dnsutils bc python-pip python-qrcode
-sudo pip install qrcode[pil]
+
+sudo apt-get -y install netcat-openbsd <<< y
+
+sudo apt-get update && sudo apt-get upgrade <<< y
+
+sudo apt-get install python git unzip pv jq <<< y
+
+sudo apt-get install python git unzip pv jq dnsutils <<< y
+
+sudo apt install bc <<< y
+
+sudo apt install python-pip <<< y
+
+sudo pip install qrcode[pil] <<< y
+
+sudo apt install python-qrcode <<< y
 
 clear
 
