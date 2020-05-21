5231138869d056ee1c8677ca5a598697c4ecbbd2
GBen1 <31903813+GBen1@users.noreply.github.com>
2020-05-21 08:43:19 +0200

    Update watch_update.sh

--- watch_update.sh
+++ watch_update.sh
@@ -64,7 +64,7 @@ echo ""
 echo -e "${gr}ACTIVE SCRIPTS${neutre}"
 
 a=0
-parttoanon=$(ps -ef | grep bash | grep parttoanon | cut -c49-)
+parttoanon=$(ps -ef | grep bash | grep parttoanon |  sed 's/.*bash -c//')
 chps=$(echo $parttoanon | wc -c)
 if ((chps > 1 ));
 then
@@ -73,7 +73,7 @@ echo -e "${yel}$parttoanon${neutre}"
 ((++a))
 fi
 
-anontoanon=$(ps -ef | grep bash | grep anontoanon | cut -c49-)
+anontoanon=$(ps -ef | grep bash | grep anontoanon |  sed 's/.*bash -c//')
 chps=$(echo $anontoanon | wc -c)
 if ((chps > 1 ));
 then
@@ -83,7 +83,7 @@ echo -e "${yel}$anontoanon${neutre}"
 fi
 
 
-anontoblind=$(ps -ef | grep bash | grep anontoblind | cut -c49-)
+anontoblind=$(ps -ef | grep bash | grep anontoblind |  sed 's/.*bash -c//')
 chps=$(echo $anontoblind | wc -c)
 if ((chps > 1 ));
 then
@@ -94,7 +94,7 @@ echo -e "${yel}$anontoblind${neutre}"
 fi
 
 
-anontopart=$(ps -ef | grep bash | grep anontopart | cut -c49-)
+anontopart=$(ps -ef | grep bash | grep anontopart |  sed 's/.*bash -c//')
 chps=$(echo $anontopart| wc -c)
 if ((chps > 1 ));
 then
@@ -103,7 +103,7 @@ echo -e "${yel}$anontopart${neutre}"
 ((++a))
 fi
 
-autoupdate=$(ps -ef | grep bash | grep "partyman update" | cut -c49-)
+autoupdate=$(ps -ef | grep bash | grep "partyman update" |  sed 's/.*bash -c//')
 chps=$(echo $autoupdate| wc -c)
 if ((chps > 1 ));
 then
