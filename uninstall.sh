2b750888fc6d2d17f080cb9be69289a46fe57d62
GBen1 <31903813+GBen1@users.noreply.github.com>
2020-05-21 08:35:03 +0200

    Update uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -1,35 +1,35 @@
 #!/bin/bash
 
 cd
-script1=$(ps -ef | grep bash | grep script1.sh | sed 's/.*-c//')
+script1=$(ps -ef | grep bash | grep script1.sh | cut -c10-14)
 num=$(echo $script1 | wc -w)
 x=1; while [ $x -le $num ]; do kill=$(ps -ef | grep bash | grep script1.sh | cut -c10-14 | sed -n "1p") && sudo kill -9 $kill $(( x++ )); done
 
-script2=$(ps -ef | grep bash | grep script2.sh | sed 's/.*-c//')
+script2=$(ps -ef | grep bash | grep script2.sh | cut -c10-14)
 num=$(echo $script2 | wc -w)
 x=1; while [ $x -le $num ]; do kill=$(ps -ef | grep bash | grep script2.sh | cut -c10-14 | sed -n "1p") && sudo kill -9 $kill $(( x++ )); done
 
-parttoanon=$(ps -ef | grep bash | grep sendparttoanon | sed 's/.*-c//')
+parttoanon=$(ps -ef | grep bash | grep sendparttoanon | cut -c10-14)
 num=$(echo $parttoanon | wc -w)
 x=1; while [ $x -le $num ]; do kill=$(ps -ef | grep bash | grep sendparttoanon | cut -c10-14 | sed -n "1p") && sudo kill -9 $kill $(( x++ )); done
 
-anontopart=$(ps -ef | grep bash | grep anontopart | sed 's/.*-c//')
+anontopart=$(ps -ef | grep bash | grep anontopart | cut -c10-14)
 num=$(echo $anontopart | wc -w)
 x=1; while [ $x -le $num ]; do kill=$(ps -ef | grep bash | grep sendanontopart | cut -c10-14 | sed -n "1p") && sudo kill -9 $kill $(( x++ )); done
 
-anontoblind=$(ps -ef | grep bash | grep anontoblind | sed 's/.*-c//')
+anontoblind=$(ps -ef | grep bash | grep anontoblind | cut -c10-14)
 num=$(echo $anontoblind | wc -w)
 x=1; while [ $x -le $num ]; do kill=$(ps -ef | grep bash | grep sendanontoblind | cut -c10-14 | sed -n "1p") && sudo kill -9 $kill $(( x++ )); done
 
-anontoanon=$(ps -ef | grep bash | grep anontoanon | sed 's/.*-c//')
+anontoanon=$(ps -ef | grep bash | grep anontoanon | cut -c10-14)
 num=$(echo $anontoanon | wc -w)
 x=1; while [ $x -le $num ]; do kill=$(ps -ef | grep bash | grep sendanontoanon | cut -c10-14 | sed -n "1p") && sudo kill -9 $kill $(( x++ )); done
 
-script3=$(ps -ef | grep bash | grep script3.sh| sed 's/.*-c//')
+script3=$(ps -ef | grep bash | grep script3.sh | cut -c10-14)
 num=$(echo $script3 | wc -w)
 x=1; while [ $x -le $num ]; do kill=$(ps -ef | grep bash | grep script3.sh | cut -c10-14 | sed -n "1p") && sudo kill -9 $kill $(( x++ )); done
 
-update=$(ps -ef | grep bash | grep "partyman update" | sed 's/.*-c//')
+update=$(ps -ef | grep bash | grep "partyman update" | cut -c10-14)
 num=$(echo $update | wc -w)
 x=1; while [ $x -le $num ]; do kill=$(ps -ef | grep bash | grep "partyman update" | cut -c10-14 | sed -n "1p") && sudo kill -9 $kill $(( x++ )); done
 
